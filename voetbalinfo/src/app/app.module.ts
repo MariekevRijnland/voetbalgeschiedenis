1503427e29d0ea100128465e09ceb7ed96befc3f
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-11-25 10:30:05 +0100

    Nieuwe componenten aangemaakt

--- voetbalinfo/src/app/app.module.ts
+++ voetbalinfo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TeamsComponent } from './teams/teams.component';
+import { SpelersComponent } from './spelers/spelers.component';
+import { MatchesComponent } from './matches/matches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TeamsComponent,
+    SpelersComponent,
+    MatchesComponent
   ],
   imports: [
     BrowserModule,
