d7b4648a1e86c7edc8fd3ebdbbbefcbb7e2e59d0
SiwaniV <6003716@mborijnland.nl>
2022-12-09 11:38:46 +0100

    pagination toegevoegd voor matches pagina

--- voetbalinfo/src/app/app.component.ts
+++ voetbalinfo/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'voetbalinfo';
+export class AppComponent implements OnInit {
+  POSTS: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 7;
+  tableSizes: any = [3, 6, 9, 12];
+  constructor(private postService: PostService) {}
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
+  fetchPosts(): void {
+    this.postService.getMatchPage().subscribe(
+      (response) => {
+        this.POSTS = response;
+        console.log(response);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.fetchPosts();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.fetchPosts();
+  }
 }
--- voetbalinfo/src/app/matches/matches.component.ts
+++ voetbalinfo/src/app/matches/matches.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { JsonService } from '../json.service';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-matches',
@@ -9,10 +9,38 @@ import { JsonService } from '../json.service';
 export class MatchesComponent implements OnInit {
   
   matches:any;
-  constructor(private json: JsonService) { }
+  PostService: any;
+  constructor(private json: PostService) { }
 
   ngOnInit(): void {
-    this.json.getmatches().subscribe(json => this.matches = json)
+    this.json.getMatchPage().subscribe(json => this.matches = json)
+  }
+
+  POSTS: any;
+  page: number = 1;
+  count: number = 0;
+  tableSize: number = 7;
+  tableSizes: any = [3, 6, 9, 12];
+
+  fetchPosts(): void {
+    this.PostService.getMatchPage().subscribe(
+      (response: any) => {
+        this.POSTS = response;
+        console.log(response);
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+  onTableDataChange(event: any) {
+    this.page = event;
+    this.fetchPosts();
+  }
+  onTableSizeChange(event: any): void {
+    this.tableSize = event.target.value;
+    this.page = 1;
+    this.fetchPosts();
   }
 
 }
