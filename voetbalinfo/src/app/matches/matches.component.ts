6f918f09836434dcffb4dd2007b152c66843afb8
Marieke van Rijn <74531663+MariekevRijnland@users.noreply.github.com>
2022-12-23 14:25:51 +0100

    bestanden terug gezet
    help met footer plz

--- voetbalinfo/src/app/matches/matches.component.ts
+++ voetbalinfo/src/app/matches/matches.component.ts
@@ -1,10 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { JsonService } from '../json.service';
 import { ActivatedRoute } from '@angular/router';
-=======
-import { PostService } from '../post.service';
->>>>>>> parent of a499e20 (Merge branch 'main' into pagination-Siwani)
 
 @Component({
   selector: 'app-matches',
@@ -13,48 +9,14 @@ import { PostService } from '../post.service';
 })
 export class MatchesComponent implements OnInit {
   
+  year:any;
   matches:any;
-<<<<<<< HEAD
  
   constructor(private json: JsonService, private route:ActivatedRoute) { 
     this.route.params.subscribe(params => this.year = params['year']);
   }
   ngOnInit(): void {
     this.json. getMatchesByYear(this.year).subscribe(json => this.matches = json)
-=======
-  PostService: any;
-  constructor(private json: PostService) { }
-
-  ngOnInit(): void {
-    this.json.getMatchPage().subscribe(json => this.matches = json)
-  }
-
-  POSTS: any;
-  page: number = 1;
-  count: number = 0;
-  tableSize: number = 7;
-  tableSizes: any = [3, 6, 9, 12];
-
-  fetchPosts(): void {
-    this.PostService.getMatchPage().subscribe(
-      (response: any) => {
-        this.POSTS = response;
-        console.log(response);
-      },
-      (error: any) => {
-        console.log(error);
-      }
-    );
-  }
-  onTableDataChange(event: any) {
-    this.page = event;
-    this.fetchPosts();
-  }
-  onTableSizeChange(event: any): void {
-    this.tableSize = event.target.value;
-    this.page = 1;
-    this.fetchPosts();
->>>>>>> parent of a499e20 (Merge branch 'main' into pagination-Siwani)
   }
 
 }
--- voetbalinfo/src/app/spelers/spelers.component.ts
+++ voetbalinfo/src/app/spelers/spelers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonService } from '../json.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-spelers',
@@ -7,10 +8,19 @@ import { JsonService } from '../json.service';
   styleUrls: ['./spelers.component.scss']
 })
 export class SpelersComponent implements OnInit {
-  players:any;
-  constructor(private json: JsonService) { }
+
+  country:any;
+  matchID:any;
+  spelers:any;
+
+  constructor(private json: JsonService, private route:ActivatedRoute) { 
+    this.route.params.subscribe(params => this.matchID = params['matchid']);
+    this.route.params.subscribe(params => this.country = params['country']);
+  }
+
   ngOnInit(): void {
       // Haalt alle jaren op die in de json query staan
-    this.json.getPlayers().subscribe(json => this.players = json)
+    this.json.getPlayersByID(this.matchID).subscribe(json => this.spelers = json)
+    this.json.getFlag(this.country).subscribe(json => this.spelers = json)
   }
 }
